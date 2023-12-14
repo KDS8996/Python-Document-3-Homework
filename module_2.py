5210b37e7c508d2bbf3761a17de63cc369c2f0f3
KDS8996 <150230820+KDS8996@users.noreply.github.com>
2023-12-14 16:35:25 -0500

    Add files via upload

---
+++ module_2.py
@@ -0,0 +1,8 @@
+
+def calculate_square_footage(length, width):
+    area = length * width
+    return area
+
+def calculate_circumference(radius):
+    circumference = 2 * 3.14 * radius
+    return circumference
